da9aff8a88bd4a5b8cfb83e0f4b99808d5b7baab
SChaotyx <agustinangelcab@gmail.com>
2025-12-15 11:52:46 -0300

    daily chest

--- config/dailyChests.php
+++ config/dailyChests.php
@@ -7,21 +7,21 @@
 	REWARDS
 */
 //SMALL CHEST
-$chest1minOrbs = 200;
-$chest1maxOrbs = 400;
-$chest1minDiamonds = 2;
-$chest1maxDiamonds = 10;
-$chest1items = [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14];
+$chest1minOrbs = 2000;
+$chest1maxOrbs = 4000;
+$chest1minDiamonds = 20;
+$chest1maxDiamonds = 100;
+$chest1items = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 $chest1minKeys = 1;
 $chest1maxKeys = 6;
 //BIG CHEST
-$chest2minOrbs = 2000;
-$chest2maxOrbs = 4000;
-$chest2minDiamonds = 20;
-$chest2maxDiamonds = 100;
-$chest2items = [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14];
-$chest2minKeys = 1;
-$chest2maxKeys = 6;
+$chest2minOrbs = 20000;
+$chest2maxOrbs = 40000;
+$chest2minDiamonds = 200;
+$chest2maxDiamonds = 1000;
+$chest2items = [10, 12, 13, 14, 15, 16, 20, 21, 22, 23, 24];
+$chest2minKeys = 10;
+$chest2maxKeys = 26;
 //REWARD TIMES (in seconds)
 $chest1wait = 3600;
 $chest2wait = 14400;
