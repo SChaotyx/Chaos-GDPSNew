4fd1c12ac80f96defe53716d0ddbc0fb765bf682
SChaotyx <agustinangelcab@gmail.com>
2026-01-17 01:56:06 -0300

    little fix daily/weekly command

--- incl/lib/commands.php
+++ incl/lib/commands.php
@@ -144,15 +144,17 @@ class Commands {
 					$query->execute([':levelID' => $levelID, ':uploadDate' => $timestamp]);
 				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account, value2, value4) VALUES ('5', :value, :levelID, :timestamp, :id, :dailytime, 0)");
 				$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID, ':dailytime' => $timestamp]);
+				$date = date("d/m/Y", $timestamp - 1);
+				$dis->discordNotifyNew(1, $levelID, 1, 4, 11, 6, $accountID, 1, 0, $date);
 				return true;
 			}
 			//weekly
 			if(substr($comment,0,7) == '!weekly'){
-				$query = $db->prepare("SELECT count(*) FROM dailyfeatures WHERE levelID = :level AND type = 1");
-				$query->execute([':level' => $levelID]);
-				if($query->fetchColumn() != 0){
-					return false;
-				}
+				//$query = $db->prepare("SELECT count(*) FROM dailyfeatures WHERE levelID = :level AND type = 1");
+				//$query->execute([':level' => $levelID]);
+				//if($query->fetchColumn() != 0){
+				//	return false;
+				//}
 				$query = $db->prepare("SELECT timestamp FROM dailyfeatures WHERE timestamp >= :tomorrow AND type = 1 ORDER BY timestamp DESC LIMIT 1");
 					$query->execute([':tomorrow' => strtotime("next monday")]);
 				if($query->rowCount() == 0){
@@ -164,6 +166,8 @@ class Commands {
 				$query->execute([':levelID' => $levelID, ':uploadDate' => $timestamp]);
 				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account, value2, value4) VALUES ('5', :value, :levelID, :timestamp, :id, :dailytime, 1)");
 				$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID, ':dailytime' => $timestamp]);
+				$date = date("d/m/Y", $timestamp - 1);
+				$dis->discordNotifyNew(1, $levelID, 1, 4, 12, 6, $accountID, 1, 0, $date);
 				return true;
 			}			
 		}
